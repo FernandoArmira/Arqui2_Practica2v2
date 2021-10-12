d5a8e6eb7c64136c923efcb5d2e9f01bbad39e5c
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2021-10-12 13:13:10 -0600

    correccion

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const  port = process.env.PORT || 3000;
 
 //app.set('port', 3000)
-app.use(cors({ origin: true, optionsSuccessStatus: 200 }));
+app.use(cors());
 
 app.listen(port, () => {
     console.log('Servidor corriendo en el puerto', port)
