7232c69506e075cc00562f8ea663d9461aec63f0
FernandoArmira <2864934620101@ingenieria.usac.edu.gt>
2021-10-13 01:56:06 -0600

    status completo

--- index.js
+++ index.js
@@ -88,6 +88,10 @@ function statusData(req,res){
     var viento = 0
     var humedad = 0
     var luz = 0
+    var calor = ""
+    var velocidadviento = ""
+    var lluvia = ""
+    var visibilidad = ""
     MongoClient.connect(url, function(err, db){
         if (err) throw err; 
         const dbo = db.db ('mydb'); 
@@ -95,9 +99,47 @@ function statusData(req,res){
         //dbo.collection ('medidas').findOne({Medida:'humedad'}, {sort:{$natural:-1}},function(err, doc){  //Filtrar datos por medida
             if(err) throw err;
             console.log(doc);
-            dato = doc;
+            //dato = doc;
             temperatura = doc.mediatemperatura
+            viento = doc.mediaviento
+            humedad = doc.mediahumedad
+            luz = doc.medialuz
             console.log(temperatura)
+            console.log(viento)
+            console.log(humedad)
+            console.log(luz)
+
+            if(viento >= 0 &&  viento <= 20){
+                velocidadviento = "Normal"
+            }else if (viento > 20 && viento <=40){
+                velocidadviento = "Moderado"
+            }else {
+                velocidadviento = "Alto"
+            }
+
+            if(luz >= 0 &&  luz <= 30){
+                visibilidad = "Nublado"
+            }else if (luz > 30 && luz <= 60){
+                visibilidad = "Medio nublado"
+            }else {
+                visibilidad = "Despejado"
+            }
+
+
+            if(humedad >= 75 ){
+                lluvia = "Con lluvia"
+            }else{
+                lluvia = "Sin lluvia"
+            }
+
+            if(temperatura >= 27 ){
+                calor = "Con calor"
+            }else{
+                calor = "Sin calor"
+            }
+
+            dato = "{\"Velocidadviento\": \"" + velocidadviento + "\", \"Visibilidad\": \"" + visibilidad + "\" , \"Lluvia\": \"" + lluvia + "\" , \"Calor\": \"" + calor + "\"}"
+
             res.send(dato)
             //Obtener datos del json
             /*console.log("Temperatura:" + doc.temperatura);
